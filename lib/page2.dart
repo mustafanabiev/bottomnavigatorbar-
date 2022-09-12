71f21124f996f5b174357cf160b0fabfce013cd4
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-09-12 11:49:45 +0600

    first commit

---
+++ lib/page2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+      body:  Center(
+        child: Text('2 - page'),
+      ),
+    );
+  }
+}
